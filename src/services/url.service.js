0c6d53f2fb11d886227a3dff2293fd18c0d16911
abonnet <anthony.bonnet1@orange.fr>
2022-06-28 13:29:45 +0200

    Appell https

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -1,7 +1,7 @@
-export const API = 'http://164.132.55.64:3332/api'
-export const API_IMAGES = 'http://164.132.55.64:3332/api/images'
-export const NO_IMAGE = 'http://164.132.55.64:3332/api/images/29fd9cba1ac0f8b2bf928eafb954e4f4.png'
+export const API = 'https://164.132.55.64:3332/api'
+export const API_IMAGES = 'https://164.132.55.64:3332/api/images'
+export const NO_IMAGE = 'https://164.132.55.64:3332/api/images/29fd9cba1ac0f8b2bf928eafb954e4f4.png'
 
-// export const API = 'http://localhost:3332/api'
-// export const API_IMAGES = 'http://localhost:3332/api/images'
-// export const NO_IMAGE = 'http://localhost:3332/api/images/29fd9cba1ac0f8b2bf928eafb954e4f4.png'
+// export const API = 'https://localhost:3333/api'
+// export const API_IMAGES = 'https://localhost:3332/api/images'
+// export const NO_IMAGE = 'https://localhost:3332/api/images/29fd9cba1ac0f8b2bf928eafb954e4f4.png'
